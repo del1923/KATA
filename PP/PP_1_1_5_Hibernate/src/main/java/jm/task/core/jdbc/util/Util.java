b0a1cd593f6b930e754ddaed7bb96bb191816fb8
Dmitry Khakhalev <khakhalev@mail.ru>
2023-02-04 20:23:27 +0300

    В Util добавлена SessionFactory

--- PP/PP_1_1_5_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP/PP_1_1_5_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,16 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+//import java.sql.Connection;
+//import java.sql.Driver;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
     /*
@@ -26,8 +33,11 @@ public class Util {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory = null;
 
+    /*
     static { //используем статический блок инициализации
+
         checkDriver();
     }
 
@@ -50,6 +60,32 @@ public class Util {
             throw new RuntimeException(e);
         }
     }
+    */
+    // описываем SessionFactory
+    public static SessionFactory getConnection() {
+
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", USER)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size","5")
+                    .setProperty("hibernate.c3p0.max_size","200")
+                    .setProperty("hibernate.c3p0.max_statements","200");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
+
+
 }
 
 
